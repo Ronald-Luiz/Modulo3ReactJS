39c18c7d072aa608f79379d4f0f29729d78c81c9
Leonardo Macena <113394226+leomacena@users.noreply.github.com>
2022-12-12 18:59:07 -0300

    Update Contatos.jsx

--- src/view/Contatos.jsx
+++ src/view/Contatos.jsx
@@ -24,7 +24,7 @@ class HelloForm extends React.Component {
         document.title="Contatos"
         return (
 
-
+        <div>
             <div className="card blue">
          
           
@@ -38,9 +38,68 @@ class HelloForm extends React.Component {
                 </div>
                 <button className="botao" onClick={this.handleClick.bind(this)}>Enviar</button>
                 
-            </div>          
+            </div>   
+
+             <div class="main texto" style="margin-top: 45px;">
+    <div class="card-container">
+        <div class="cards">
+          <div class="avatar"><img src="img/caio.jpeg"></div>
+          <p><h4>CAIO PEREIRA</h4></p>
+          <p><h5>Função na equipe</h5></p>
+          <div class="social">
+            <a href="https://github.com/caaiopereira" target="_blank"><img src="img/git.png"></a>
+            <a href="https://instagram.com/kingscaio" target="_blank"><img src="img/inst.png"></a>
+            <a href="https://www.linkedin.com/in/caio-pereira-oliveira/" target="_blank"><img src="img/linkedin.png" ></a>
+          </div>
+        </div>
+   
+        <div class="cards">
+          <div class="avatar"><img src="img/daniel.jpg" alt="Perfil"></div>
+          <p><h4>DIÓGENES DA SILVA</h4></p>
+          <p><h5>Função na equipe</h5></p>
+          <div class="social">
+            <a href="https://github.com/diogenesferreirabr" target="_blank"><img src="img/git.png" alt="GitHub"></a>
+            <a href="" target="_blank"><img src="img/inst.png" alt="Instagram"></a>
+            <a href="https://www.linkedin.com/in/diogenes-ferreira-6aab9324a/" target="_blank"><img src="img/linkedin.png" alt="LinkedIn"></a>
+          </div>
+        </div>
+
+        <div class="cards">
+          <div class="avatar"><img src="img/leonardo.jpg" alt="Perfil"></div>
+          <p><h4>LEONARDO RODRIGUES</h4></p>
+          <p><h5>Função na equipe</h5></p>
+          <div class="social">
+            <a href="https://github.com/leomacena" target="_blank"><img src="img/git.png" alt="GitHub"></a>
+            <a href="https://instagram.com/leomacena23" target="_blank"><img src="img/inst.png" alt="Instagram"></a>
+            <a href="https://www.linkedin.com/in/leonardomacena/" target="_blank"><img src="img/linkedin.png" alt="LinkedIn"></a>
+          </div>
+        </div>
+
+        <div class="cards">
+          <div class="avatar"><img src="img/luiz.jpg" alt="Perfil"></div>
+          <p><h4>LUÍS HENRIQUE</h4></p>
+          <p><h5>Função na equipe</h5></p>
+          <div class="social">
+            <a href="https://github.com/Luis-Henrique-Lima" target="_blank"><img src="img/git.png" alt="GitHub"></a>
+            <a href="" target="_blank"><img src="img/inst.png" alt="Instagram"></a>
+            <a href="https://www.linkedin.com/in/lu%C3%ADs-henrique-santos/" target="_blank"><img src="img/linkedin.png" alt="LinkedIn"></a>
+          </div>
+        </div>
+
+        <div class="cards">
+          <div class="avatar"><img src="img/pedro.jpg" alt="Perfil"></div>
+          <p><h4>PEDRO DE SOUZA</h4></p>
+          <p><h5>Função na equipe</h5></p>
+          <div class="social">
+            <a href="https://github.com/themonsteer" target="_blank"><img src="img/git.png" alt="GitHub"></a>
+            <a href="" target="_blank"><img src="img/inst.png" alt="Instagram"></a>
+            <a href="https://www.linkedin.com/in/pedro-souza-a382b3182" target="_blank"><img src="img/linkedin.png" alt="LinkedIn"></a>
+          </div>
+        </div>       
+    </div>
+  </div>
           
-          
+          </div>
            
         )
     }
