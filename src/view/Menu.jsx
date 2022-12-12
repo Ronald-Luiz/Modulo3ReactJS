d9dfb840006856185d81531b78ae6235d0416c0f
Leonardo Macena <113394226+leomacena@users.noreply.github.com>
2022-12-12 19:42:37 -0300

    Update Menu.jsx

--- src/view/Menu.jsx
+++ src/view/Menu.jsx
@@ -1,62 +1,114 @@
-import { useRef } from "react";
-import {Outlet} from 'react-router-dom';
-import { FaBars, FaTimes } from "react-icons/fa";
-import logo from "../images/logo-nav.png"
-import {
-	UncontrolledDropdown,
-	DropdownToggle,
-	DropdownMenu,
-	DropdownItem,  
-  } from 'reactstrap';
-import "./css/Menu.css";
+import React from 'react';
+import SweetAlert from 'react-bootstrap-sweetalert';
+import './css/Contatos.css';
+import Swal from 'sweetalert2';
+import avatar from "../images/avatar.png"
+import insta from "../images/insta.png"
+import git from "../images/git.png"
+import linkedin from "../images/linkedin.png"
 
-function Barra() {
-	const navRef = useRef();
+class HelloForm extends React.Component {
 
-	const showNavbar = () => {
-		navRef.current.classList.toggle("responsive_nav");
-	};
 
-	return (
-    <div>
-		<header>
-			<img src={logo} alt="logo" />
-			<div>
-			<nav ref={navRef}>
-				<a href="/">INICIO</a>
-				<a href="/produtos">PRODUTOS</a>
-				<a href="/contatos">CONTATOS</a>
-				<UncontrolledDropdown
-				inNavbar
-				nav
-				>
-              <DropdownToggle nav caret className="ConfBtn">
-                CONFIGURAÇÕES
-              </DropdownToggle>
+    state = {
+        texto: ''
+    };
 
+    handleTextChange(e) {
+        this.setState({
+            texto: e.target.value
+        });
+    }
+
+    handleClick() {
+        Swal.fire('Entraremos em contato em breve!')
+    }
+
+    render() {
+        document.title = "Contatos"
+        return (
+
+            <>
+                <div className="card blue">
+
+
+
+                    <div className="teste">
+                        <strong>Nome</strong> <input type="text" onChange={this.handleTextChange.bind(this)}></input>
+                        <strong>Email</strong> <input type="text" onChange={this.handleTextChange.bind(this)}></input>
+                        <strong>Telefone</strong> <input type="text" onChange={this.handleTextChange.bind(this)}></input>
+                        <strong>Digite o texto:</strong> <input type="text" onChange={this.handleTextChange.bind(this)}></input>
+                    </div>
+                    
+                    <button className="botao" onClick={this.handleClick.bind(this)}>Enviar</button>
+
+                </div>
+
+                <div class="main texto" style="margin-top: 45px;"></div>
+
+                    <div class="card-container">
+                        <div class="cards">
+                            <div class="avatar"><img src={avatar} alt="Perfil" /></div>
+                                <p><h4>LEONARDO MACENA</h4></p>
+                                <p><h5>Função na equipe</h5></p>
+                                <div class="social">
+                                    <a href="#" target="_blank"><img src={git} alt="GitHub" /> </a>
+                                    <a href="#" target="_blank"><img src={insta} alt="Instagram" /></a>
+                                    <a href="#" target="_blank"><img src={linkedin}alt="LinkedIn" /></a>
+                                </div>
+                        </div>
+
+                        <div class="cards">
+                            <div class="avatar"><img src={avatar} alt="Perfil" /></div>
+                                    <p><h4>PABLO MICELI</h4></p>
+                                    <p><h5>Função na equipe</h5></p>
+                                    <div class="social">
+                                        <a href="#" target="_blank"><img src={git} alt="GitHub" /></a>
+                                        <a href="#" target="_blank"><img src={insta} alt="Instagram" /></a>
+                                        <a href="#" target="_blank"><img src={linkedin}alt="LinkedIn" /></a>
+                                    </div>
+                        </div>
+
+                        <div class="cards">
+                            <div class="avatar"><img src={avatar} alt="Perfil" /></div>
+                                    <p><h4>RONALD LUIZ</h4></p>
+                                    <p><h5>Função na equipe</h5></p>
+                                    <div class="social">
+                                        <a href="#" target="_blank"><img src={git} alt="GitHub" /></a>
+                                        <a href="#" target="_blank"><img src={insta} alt="Instagram" /></a>
+                                        <a href="#" target="_blank"><img src={linkedin} alt="LinkedIn" /></a>
+                                    </div>
+                        </div>
+
+                        <div class="cards">
+                            <div class="avatar"><img src={avatar} alt="Perfil" /></div>
+                                    <p><h4>VALDEILSON</h4></p>
+                                    <p><h5>Função na equipe</h5></p>
+                                    <div class="social">
+                                        <a href="#" target="_blank"><img src={git} alt="GitHub" /></a>
+                                        <a href="#" target="_blank"><img src={insta} alt="Instagram" /></a>
+                                        <a href="#" target="_blank"><img src={linkedin} alt="LinkedIn" /></a>
+                                    </div>
+                        </div>
+
+                        <div class="cards">
+                            <div class="avatar"><img src={avatar} alt="Perfil" /></div>
+                                    <p><h4>VICTOR ROBSON</h4></p>
+                                    <p><h5>Função na equipe</h5></p>
+                                    <div class="social">
+                                        <a href="#" target="_blank"><img src={git} alt="GitHub" /></a>
+                                        <a href="#" target="_blank"><img src={insta} alt="Instagram" /></a>
+                                        <a href="#" target="_blank"><img src={linkedin} alt="LinkedIn" /></a>
+                                    </div>
+                        </div>       
+                    </div>
+            
+
+        </>
         
-              <DropdownMenu className="ConfOpen" left>
-                <DropdownItem href="/read">Estoque</DropdownItem>
-                <DropdownItem href="/create">Criar</DropdownItem>
-              </DropdownMenu>
-  
 
-
-            </UncontrolledDropdown>
-				<button
-					className="nav-btn nav-close-btn"
-					onClick={showNavbar}>
-					<FaTimes />
-				</button>
-				</nav>
-			<button className="nav-btn" onClick={showNavbar}>
-				<FaBars />
-			</button>
-		</div>
-    </header>
-    <Outlet />
-    </div>
-	);
+        )
+    }
 }
 
-export default Barra;
+export default HelloForm;
