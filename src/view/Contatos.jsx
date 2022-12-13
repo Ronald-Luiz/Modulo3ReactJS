5511c0d57082d696f0d231d5ad81fc6b093d4b52
Ronald <56053290+Ronald-Luiz@users.noreply.github.com>
2022-12-13 20:12:29 -0300

    Add files via upload

--- src/view/Contatos.jsx
+++ src/view/Contatos.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import SweetAlert from 'react-bootstrap-sweetalert';
 import './css/Contatos.css'
 import Swal from 'sweetalert2'
+import InputMask from 'react-input-mask';
+import Form from 'react-bootstrap/Form';
 
 class HelloForm extends React.Component {
-    
-        
+
+
     state = {
         texto: ''
     };
@@ -21,28 +23,49 @@ class HelloForm extends React.Component {
     }
 
     render() {
-        document.title="Contatos"
+        document.title = "Contatos"
         return (
 
 
             <div className="card blue">
-         
-          
+
+
 
                 <div className="teste">
-                    <strong>Nome</strong> <input type="text" onChange={this.handleTextChange.bind(this)}></input>
-                    <strong>Email</strong> <input type="text" onChange={this.handleTextChange.bind(this)}></input>
-                    <strong>Telefone</strong> <input type="text" onChange={this.handleTextChange.bind(this)}></input>
-                    <strong>Digite o texto:</strong> <input type="text" onChange={this.handleTextChange.bind(this)}></input>
-                    
+                    <strong>Nome</strong> <Form.Control
+                        as={InputMask}
+
+                        placeholder="Digite seu nome"
+                    />
+                    <strong>Email</strong> <Form.Control
+                        as={InputMask}
+
+                        placeholder="Digite seu email"
+                    />
+
+                    <strong>Telefone</strong>
+                    <Form.Control
+                        as={InputMask}
+                        mask="(99)99999-9999"
+                        placeholder="Digite seu Telefone" />
+                    <strong>Digite o texto:</strong> <Form.Control
+                        as={InputMask}
+
+                        placeholder="Digite o texto"
+                    />
+
+
+
                 </div>
                 <button className="botao" onClick={this.handleClick.bind(this)}>Enviar</button>
-                
-            </div>          
-          
-          
-           
+
+            </div>
+
+
+
         )
+
+
     }
 }
 
