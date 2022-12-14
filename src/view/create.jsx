3c10671c96066645a324f77c709e8b7208051741
Leonardo Macena <113394226+leomacena@users.noreply.github.com>
2022-12-14 17:10:29 -0300

    Update create.jsx

--- src/view/create.jsx
+++ src/view/create.jsx
@@ -86,7 +86,7 @@ export default function Create() {
           onChange={(e) => setImagem(e.target.value)} 
           />
         </Form.Field>
-        <a href="https://modulo3-react-js.vercel.app/read"><Button type='submit' color="success" onClick={sendDataToAPI}>Inserir</Button></a>
+        <Button type='submit' color="success" onClick={sendDataToAPI}><a href="https://modulo3-react-js.vercel.app/read">Inserir</a></Button>
       </Form>
       <div className='nada'></div>
     </div>
